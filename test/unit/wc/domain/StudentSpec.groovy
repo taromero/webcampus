efc2c45792d110e9ee771b4dc355c777e7e62fb4
Mauro Gabriel Ponce <mgp1989@gmail.com>
2012-09-15 20:13:57 -0300

    Agregado test, revisar

--- test/unit/wc/domain/StudentSpec.groovy
+++ test/unit/wc/domain/StudentSpec.groovy
@@ -7,16 +7,32 @@ import spock.lang.Specification
 @Build(Student)
 class StudentSpec extends Specification{
 	
-	def 'it should not allow students with wrong mails'(){
+	def 'it should not allow to save students with wrong mails'(){
 		given:
 			Student student = Student.build();
 			student.email = email
 		when:
 			student.save(failOnError:true, flush:true)
 		then:
-			thrown(Exception)
+			Exception ex = thrown()
+			ex.message.contains("not a valid e-mail address")
 		where:
-			email << ['pirulo', 'pirulogmail.com']             
+			email << ['pirulo2gmail.com', 'pirulgmail.com']            
 	}
-	
+	def 'it should not allow to save two students with the same doc number'(){
+		given:
+			Student student1 = Student.build()
+			student1.docNumber = docNumber
+			student1.save()
+			Student student2 = Student.build()
+			student1.docNumber = docNumber
+		when:
+			student2.save()
+		then:
+			Exception ex = thrown()
+			System.out.println(ex.message);
+			//ex.message.contains("")
+		where:
+			docNumber = 34845905
+	}	
 }
